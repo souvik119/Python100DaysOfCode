e3d17965750ab7c449456c878485ec186a690911
souvikghosh <Sayali1@>
2022-08-10 10:44:00 -0700

    using recursion and while loop to continue calculator application

---
+++ Day10/calculator_2.py
@@ -0,0 +1,46 @@
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+#dictionary with keys as symbols and values as name of operation
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+
+def calculator():
+    num1 = int(input("What's the first number? "))
+    should_continue = True
+
+    while should_continue:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation: ")
+        num2 = int(input("What's the next number? "))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        
+        
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+            num1 = answer
+        else:
+            should_continue = False
+            #use recursion here
+            calculator()
+            #also this while loop will keep on running
+
+
+calculator()
+        
