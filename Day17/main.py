e3617d452f527504f40178f577a5508b9cea0f5d
souvikghosh <Sayali1@>
2022-08-16 16:32:46 -0700

    Completed gameplay

--- Day17/main.py
+++ Day17/main.py
@@ -15,3 +15,7 @@ quiz = QuizBrain(question_bank)
 
 while quiz.still_has_questions():
     quiz.next_question()
+
+
+print("You have completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
--- Day17/quiz_brain.py
+++ Day17/quiz_brain.py
@@ -8,6 +8,7 @@ class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
+        self.score = 0
 
     
     def still_has_questions(self):
@@ -19,4 +20,17 @@ class QuizBrain:
         """Prints next question and gets user input"""
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question.text} (True/False)?: ")
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False)?: ")
+        self.check_answer(user_answer, current_question.answer)
+
+    
+    def check_answer(self, user_answer, correct_answer):
+        """Checks if user answered correctly"""
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print("You got it right!")
+        else:
+            print("That's wrong.")
+        print(f"The correct answer was: {correct_answer}.")
+        print(f"Your current score is: {self.score}/{self.question_number}")
+        print("\n")
