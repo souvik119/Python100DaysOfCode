cbecb8827e42dd19ed824bdd54603a4e68368c1d
souvikghosh <Sayali1@>
2022-08-19 17:16:38 -0700

    Completed the snake game

--- Day21-30/Day21/main.py
+++ Day21-30/Day21/main.py
@@ -1,11 +1,13 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 def setup_screen():
     """Sets up screen properties for the game"""
@@ -31,6 +33,22 @@ def main():
         #detect collision with food
         if snake.head.distance(food) < 15:
             food.refresh()
+            snake.extend()
+            scoreboard.increase_score()
+
+        #detect collision with wall
+        if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+            game_is_on = False
+            scoreboard.game_over()
+
+        #detect collision with tail
+        #if head collides with any segment in the tail then initiate game over sequence
+        for segment in snake.segments:
+            if segment == snake.head:
+                continue
+            if snake.head.distance(segment) < 10:
+                game_is_on = False
+                scoreboard.game_over()
     
     screen.exitonclick()
 
--- Day21-30/Day21/snake.py
+++ Day21-30/Day21/snake.py
@@ -19,11 +19,20 @@ class Snake:
     def create_snake(self):
         """Creates a snake and positions it at starting position"""
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+
+    def extend(self):
+        """Add new segments"""
+        self.add_segment(self.segments[-1].position())
 
     
     def move(self):
