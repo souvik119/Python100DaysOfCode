ab868f7dfe5e4a8248fe73ba34dbdc33d7294d2e
souvikghosh <Sayali1@>
2022-08-20 12:01:47 -0700

    Added paddle miss logic

--- Day21-30/Day22/ball.py
+++ Day21-30/Day22/ball.py
@@ -17,6 +17,15 @@ class Ball(Turtle):
         self.goto(new_x, new_y)
 
     
-    def bounce(self):
+    def bounce_y(self):
         self.y_move *= -1
-        
+
+    
+    def bounce_x(self):
+        self.x_move *= -1
+
+    
+    def reset_position(self):
+        self.goto(0, 0)
+        #reverse position so ball starts with going to other player
+        self.bounce_x()
--- Day21-30/Day22/main.py
+++ Day21-30/Day22/main.py
@@ -32,7 +32,21 @@ def main():
         #detect ball collision with top and bottom wall
         if ball.ycor() > 280 or ball.ycor() < -280:
             #need to bounce
-            ball.bounce()
+            ball.bounce_y()
+
+        #detect ball collision with paddle
+        if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+            #need to bounce
+            ball.bounce_x()
+
+        #detect right paddle misses the ball
+        if ball.xcor() > 380:
+            ball.reset_position()
+
+        #detect left paddle misses the ball
+        if ball.xcor() < -380:
+            ball.reset_position()
+
 
     screen.exitonclick()
 
