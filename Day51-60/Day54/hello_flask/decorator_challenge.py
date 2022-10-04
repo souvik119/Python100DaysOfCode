885048b4e1405813d6894406accfd63c535bc826
souvikghosh <Sayali1@>
2022-10-03 23:52:55 -0700

    Created a decoartor to time a function run

---
+++ Day51-60/Day54/hello_flask/decorator_challenge.py
@@ -0,0 +1,25 @@
+import time
+current_time = time.time()
+#print(current_time)
+
+def speed_calc_decorator(func):
+    def wrapper_func():
+        start_time = time.time()
+        func()
+        end_time = time.time()
+        print(f"{func.__name__} run speed: {end_time - start_time}s")
+    return wrapper_func
+
+@speed_calc_decorator
+def fast_function():
+    for i in range(10000000):
+        i * i
+
+@speed_calc_decorator     
+def slow_function():
+    for i in range(100000000):
+        i * i
+
+
+fast_function()
+slow_function()
