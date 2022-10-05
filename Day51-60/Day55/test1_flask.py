83e7e7da11a9a93a82b7a129771d4548af2774aa
souvikghosh <Sayali1@>
2022-10-05 11:55:30 -0700

    Practice for decorators

--- Day51-60/Day55/test1_flask.py
+++ Day51-60/Day55/test1_flask.py
@@ -2,6 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def make_bold(func):
+    def wrapper_func():
+        return "<b>" + func() + "</b>"
+    return wrapper_func
+
+def make_emphasis(func):
+    def wrapper_func():
+        return "<em>" + func() + "</em>"
+    return wrapper_func
+
+def make_underlined(func):
+    def wrapper_func():
+        return "<u>" + func() + "</u>"
+    return wrapper_func
+
 # homepage
 @app.route("/")
 def hello_world():
@@ -11,6 +26,13 @@ def hello_world():
 @app.route("/username/<name>/<int:number>")
 def greet(name, number):
     return f"Hello there {name}!, you are {number} years old!"
+
+@app.route("/bye")
+@make_bold
+@make_emphasis
+@make_underlined
+def bye():
+    return "Bye!"
 
 
 if __name__ == "__main__":
