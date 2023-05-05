73b74c6b238fc0b0d2448663c6f69e6e7eef3096
souvikghosh <Sayali1@>
2023-05-05 12:00:17 -0700

    string functions and examples

---
+++ Intermediate Python/string_example.py
@@ -0,0 +1,19 @@
+# strings: ordered, immutable, text representation
+# multi line string """ """
+# if in multi line string \ then means no new line character at the end of that line
+
+# get rid of white space
+my_string = '     Hello     '
+new_string = my_string.strip()
+print(new_string)
+
+# convert to upper and lower case using upper() and lower()
+# check if string starts with a certain charcater my_string.startswith('H')
+# same endswith
+# find to get starting position of a substring
+# count
+# replace
+
+# convert string to list
+# my_list = mystring.split()
+# new_string = ''.join(my_list)
